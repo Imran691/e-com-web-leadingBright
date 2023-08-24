0404730dd6c4295dc4f15de01a875b6e90769f1e
Imran691 <mimran691@gmail.com>
2023-08-24 16:27:33 +0500

    all products page commented out

--- src/app/allProducts/page.tsx
+++ src/app/allProducts/page.tsx
@@ -1,21 +1,21 @@
-import React from "react";
-import BASE_PATH_FORAPI from "../components/shared/BasePath";
-import { error } from "console";
-import ALLProductsCompo from "../components/views/AllProducts";
+// import React from "react";
+// import BASE_PATH_FORAPI from "../components/shared/BasePath";
+// import { error } from "console";
+// import ALLProductsCompo from "../components/views/AllProducts";
 
-async function fetchAllProductData() {
-  let res = await fetch(`${BASE_PATH_FORAPI}/api/products?start=0&end=10`);
-  // let data = await res.json()
-  // console.log(data)
-  if (!res.ok) {
-    throw new Error("Failed to fetch");
-  }
-  return res.json();
-}
+// async function fetchAllProductData() {
+//   let res = await fetch(`${BASE_PATH_FORAPI}/api/products?start=0&end=10`);
+//   // let data = await res.json()
+//   // console.log(data)
+//   if (!res.ok) {
+//     throw new Error("Failed to fetch");
+//   }
+//   return res.json();
+// }
 
-const allProducts = async () => {
-  const ProductData = await fetchAllProductData();
-  return <ALLProductsCompo ProductsData={ProductData} />;
-};
+// const allProducts = async () => {
+//   const ProductData = await fetchAllProductData();
+//   return <ALLProductsCompo ProductsData={ProductData} />;
+// };
 
-export default allProducts;
+// export default allProducts;

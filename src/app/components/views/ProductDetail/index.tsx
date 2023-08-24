8b0a8b44f4264743b4a02c5d4f660dc61fbff0cf
Imran691 <mimran691@gmail.com>
2023-08-24 16:04:32 +0500

    product detail page completed

--- src/app/components/views/ProductDetail/index.tsx
+++ src/app/components/views/ProductDetail/index.tsx
@@ -8,6 +8,7 @@ import {
 import { client } from "../../../../../sanity/lib/client";
 import ImageUrlBuilder from "@sanity/image-url";
 import Image from "next/image";
+import { BsCart2 } from "react-icons/bs";
 
 const builder: any = ImageUrlBuilder(client);
 function urlFor(source: any) {
@@ -15,19 +16,30 @@ function urlFor(source: any) {
 }
 
 const ProductDetail: FC<{ item: oneProductType }> = ({ item }) => {
-  const [imageForPreviewOfSelected, setImageForPreviewOfSelected] =
-    useState<string>(item.image[0]._key);
+
+  const [imageForPreviewOfSelected, setImageForPreviewOfSelected] = useState<string>(item.image[0]._key);
+  const [quantity, setQuantity] = useState(1)
+
+  function incrementTheQuantity(){
+    setQuantity(quantity + 1)
+  }
+
+  function decrementTheQuantity(){
+    if(quantity !== 0) {
+      setQuantity(quantity - 1)
+    }
+  }
 
   return (
-    <div>
+    <div className="flex flex-col lg:flex-row justify-center items-center">
       {/* left portion */}
-      <div className="flex gap-x-8">
+      <div className="flex gap-x-4 md:gap-x-8">
         {/* left sub portion */}
         <div className="space-y-4">
           {item.image.map((subItem: imagesType, index: number) => (
             <div
               key={index}
-              className="w-24"
+              className="w-16 md:w-24"
               onClick={() => setImageForPreviewOfSelected(subItem._key)}
             >
               <Image
@@ -40,7 +52,7 @@ const ProductDetail: FC<{ item: oneProductType }> = ({ item }) => {
           ))}
         </div>
         {/* right sub portion */}
-        <div className="w-[33rem]">
+        <div className="w-[33rem] flex flex-nowrap">
           {item.image.map((subItem: imagesType, index: number) => {
             if (subItem._key === imageForPreviewOfSelected) {
               return (
@@ -57,7 +69,40 @@ const ProductDetail: FC<{ item: oneProductType }> = ({ item }) => {
       </div>
 
       {/* right portion */}
-      <div></div>
+      <div className="p-6 space-y-8">
+        <div>
+          <h1 className="text-3xl text-gray-700">{item.productName}</h1>
+          <p className="text-xl text-purple-900 font-medium">{item.productTypes[1]}</p>
+        </div>
+        <div className="space-y-2">
+          <p className="text-lg font-semibold text-gray-700">SELECT SIZE</p>
+          <div className="flex gap-2 text-purple-900">
+            {
+              item.size.map( (subItem: string, index:number) => (
+                <div className="flex items-center justify-center w-12 h-12 rounded-full bg-gray-100 cursor-pointer font-semibold hover:shadow-xl">{subItem}</div>
+              ))
+            }
+          </div>
+        </div>
+        <div className="flex space-x-7">
+          <p className="font-semibold text-lg text-gray-700">Quantity:</p>
+          <div className="flex items-center gap-2 text-lg">
+            <div
+            onClick={decrementTheQuantity}
+            className="flex items-center justify-center rounded-full border border-gray-800 w-9 h-9 cursor-pointer select-none">-</div>
+            <p>{quantity}</p>
+            <div 
+            onClick={incrementTheQuantity}
+            className="flex items-center justify-center rounded-full border border-gray-800 w-9 h-9 cursor-pointer select-none">+</div>
+          </div>
+        </div>
+        <div className="flex items-center gap-x-8">
+        <button className="flex  items-center gap-x-2 text-white bg-gray-900 border border-gray-500 px-4 py-2">
+          <BsCart2 />
+          Add to cart</button>
+        <p className="text-2xl font-semibold">${item.price}.00</p>
+        </div>
+      </div>
     </div>
   );
 };
